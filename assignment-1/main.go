81e4f5fbf5f0302afc8d3821a07d60aeaaab2803
ZopDev <zopdev@172-0-109-171.lightspeed.mmphtn.sbcglobal.net>
2025-06-16 17:04:57 +0530

    added inside assignment-1

---
+++ assignment-1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Currency Map
+var rates = map[string]float64{
+	"USD": 1.0,
+	"INR": 83.12,
+	"EUR": 13.0,
+	"JPY": 10.0,
+}
+
+// Variables Needed
+var amount float64
+var source_currency string
+var target_currency string
+
+// Function for Taking Input
+func takeInput() {
+	fmt.Println("Enter Amount")
+	fmt.Scan(&amount)
+
+	fmt.Println("Enter Source Currency")
+	fmt.Scan(&source_currency)
+
+	fmt.Println("Enter Target Currency")
+	fmt.Scan(&target_currency)
+
+}
+
+// Function for Main Conversion Logic
+func convert() float64 {
+	return (amount / rates[source_currency]) * rates[target_currency]
+}
+
+// Main Function
+func main() {
+	var s float64
+	fmt.Println(s)
+
+	//----------------------------------------Currency Convertor-----------------------------
+	takeInput()
+
+	var totalSum float64
+	totalSum = convert()
+
+	fmt.Println(amount, source_currency, "is equivalent to", totalSum, target_currency)
+}
